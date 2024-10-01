0f02ae465e0300e9ea1c6fbadcb296633bc4260b
UmaidBinZubair <umaidbinzubair@gmail.com>
2024-10-01 10:51:10 +0200

    update folder structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.3"
+REPO_NAME = "lblimg2coords"
+PKG_NAME = "lblimg2coords"
+AUTHOR_USER_NAME = "umaidbinzubair"
+AUTHOR_EMAIL = "umaidbinzubair@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for converting labelled images to \
+        coordinates of different formats",
+    long_description=open('README.md').read(),
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":
+            f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={
+        'console_scripts': [
+            'convert_contours=lblimg2coords.convert:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+    )
